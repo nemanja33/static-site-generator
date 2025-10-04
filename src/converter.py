c4551cabad39be28f7552962c5f1465c2c7284d3
nemanja33 <djokic.nemanja97@gmail.com>
2025-10-04 08:48:43 +0200

    finished converting text to nodes

--- src/converter.py
+++ src/converter.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
   if (isinstance(old_nodes, list) != True):
@@ -12,17 +13,98 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     if (delimiter not in node.text):
       node = TextNode(node.text, node.type)
       new_nodes.extend([node])
-      continue 
-    fist_split = node.text.split(delimiter, 1)
-    if (delimiter not in fist_split[1]):
-      raise Exception("Invalid markdown!")
-    second_split = fist_split[1].split(delimiter, 1)
-    node_two = TextNode(second_split[0], text_type)
-    node_three = TextNode(second_split[1], TextType.PLAIN)
-    if (fist_split[0] != ''):
-      node_one = TextNode(fist_split[0], TextType.PLAIN)
-      new_nodes.extend([node_one, node_two, node_three])
-    else:
-      new_nodes.extend([node_two, node_three])
-  return new_nodes
+      continue
   
+    delimiter_part_one = list(filter(None, node.text.split(delimiter, 1)))
+    if (delimiter not in delimiter_part_one[1]):
+      raise Exception("Invalid markdown!")
+    
+    delimeter_part_two = list(filter(None, delimiter_part_one[1].split(delimiter, 1)))
+    
+    # nodes 
+    node_one = TextNode(delimiter_part_one[0], TextType.PLAIN)
+    node_two = TextNode(delimeter_part_two[0], text_type)
+    rest = TextNode(delimeter_part_two[1], TextType.PLAIN)
+
+    # recursevily apply to add instances with delimeter
+    if delimiter in rest.text:
+      node_three = split_nodes_delimiter([rest], delimiter, text_type)
+    else:
+      node_three = [rest]
+
+    new_nodes.extend([node_one, node_two])
+    new_nodes.extend(node_three)
+  return new_nodes
+
+def extract_markdown_images(text):
+  match = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+  return match
+  
+def extract_markdown_links(text):
+  match = re.findall(r"\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+  return match
+
+
+def split_nodes_image(old_nodes):
+  if (isinstance(old_nodes, list) != True):
+    raise Exception("Please submit an array of elements") 
+  if (len(old_nodes) == 0):
+    raise Exception("Empty list not allowed!")
+  new_nodes = []
+  for node in old_nodes:
+    extracted_images = extract_markdown_links(node.text)
+    if len(extracted_images) == 0:
+      new_nodes.append(node)
+      return new_nodes
+    extracted_image = extracted_images[0]
+    extracted_image_text = f"![{extracted_image[0]}]({extracted_image[1]})"
+    if (extracted_image_text not in node.text):
+      node = TextNode(node.text, node.type)
+      new_nodes.extend([node])
+      continue
+
+    parted_array = list(filter(None, (node.text.split(extracted_image_text, 1))))
+    
+    node_one = TextNode(parted_array[0], TextType.PLAIN)
+    
+    node_two = TextNode(extracted_image[0], TextType.IMAGE, extracted_image[1])
+    if len(parted_array) > 1:
+      node_three = split_nodes_image([TextNode(parted_array[1], TextType.PLAIN)])
+      new_nodes.extend([node_one, node_two])
+      new_nodes.extend(node_three)
+    else:
+      new_nodes.extend([node_one, node_two])
+    
+  return new_nodes
+
+def split_nodes_link(old_nodes):
+  if (isinstance(old_nodes, list) != True):
+    raise Exception("Please submit an array of elements") 
+  if (len(old_nodes) == 0):
+    raise Exception("Empty list not allowed!")
+  new_nodes = []
+  for node in old_nodes:
+    extracted_links = extract_markdown_links(node.text)
+    if len(extracted_links) == 0:
+      new_nodes.append(node)
+      return new_nodes
+    extracted_link = extracted_links[0]
+    extracted_link_text = f"[{extracted_link[0]}]({extracted_link[1]})"
+    if (extracted_link_text not in node.text):
+      node = TextNode(node.text, node.type)
+      new_nodes.extend([node])
+      continue
+
+    parted_array = list(filter(None, (node.text.split(extracted_link_text, 1))))
+    
+    node_one = TextNode(parted_array[0], TextType.PLAIN)
+    
+    node_two = TextNode(extracted_link[0], TextType.LINK, extracted_link[1])
+    if len(parted_array) > 1:
+      node_three = split_nodes_link([TextNode(parted_array[1], TextType.PLAIN)])
+      new_nodes.extend([node_one, node_two])
+      new_nodes.extend(node_three)
+    else:
+      new_nodes.extend([node_one, node_two])
+    
+  return new_nodes
--- src/test_converter.py
+++ src/test_converter.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from converter import split_nodes_delimiter
+from converter import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 
 class ConverterTest(unittest.TestCase):
   def test_plain_text(self):
@@ -60,6 +60,84 @@ class ConverterTest(unittest.TestCase):
     converted2 = split_nodes_delimiter(converted1, '**', TextType.BOLD)
     self.assertEqual(converted2, [TextNode("Let's test ", TextType.PLAIN), TextNode("code", TextType.CODE), TextNode(" text and ", TextType.PLAIN), TextNode("bold", TextType.BOLD), TextNode(" text", TextType.PLAIN)])
     
+  def test_extract_markdown_images(self):
+    matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
     
+  def test_extract_markdown_images_multi(self):
+    matches = extract_markdown_images(
+      "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and a flower ![flower](https://flower.jpg)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png"), ("flower", "https://flower.jpg")], matches)
+    
+  def test_extract_image_wrong_markdown(self):
+    matches = extract_markdown_images(
+        "This is text with an ![image(https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([], matches)
+
+  def test_extract_markdown_links(self):
+    matches = extract_markdown_links(
+        "This is text with a [link](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+  def test_extract_link_wrong_markdown(self):
+    matches = extract_markdown_links(
+        "This is text with a ![link(https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([], matches)
+    
+  def test_extract_markdown_links_multi(self):
+    matches = extract_markdown_links(
+      "This is text with a [link](https://i.imgur.com/zjjcJKZ.png) and a flower [flower](https://flower.jpg)"
+    )
+    self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png"), ("flower", "https://flower.jpg")], matches)
+    
+  def test_extract_markdown_link(self):
+    node = TextNode("This is text with a link to [boot dev](https://www.boot.dev)", TextType.PLAIN)
+    converted = split_nodes_link([node])
+    self.assertEqual(converted, [TextNode("This is text with a link to ", TextType.PLAIN), TextNode("boot dev", TextType.LINK, "https://www.boot.dev")])
+    
+      
+  def test_extract_markdown_links(self):
+    node = TextNode("This is text with a link to [boot dev](https://www.boot.dev) and to [google](google.com)", TextType.PLAIN)
+    node2 = TextNode("This is text with a link to [boot dev](https://www.boot.dev) and to [google](google.com)", TextType.PLAIN)
+    converted = split_nodes_link([node, node2])
+    self.assertEqual(converted, [TextNode("This is text with a link to ", TextType.PLAIN), TextNode("boot dev", TextType.LINK, "https://www.boot.dev"), TextNode(" and to ", TextType.PLAIN), TextNode("google", TextType.LINK, "google.com"), TextNode("This is text with a link to ", TextType.PLAIN), TextNode("boot dev", TextType.LINK, "https://www.boot.dev"), TextNode(" and to ", TextType.PLAIN), TextNode("google", TextType.LINK, "google.com")])
+
+
+  def test_empty_links(self):
+    with self.assertRaises(Exception) as context:
+      split_nodes_link([])
+    self.assertEqual(str(context.exception), "Empty list not allowed!")
+    
+  def test_non_list_links(self):
+    with self.assertRaises(Exception) as context:
+      split_nodes_link(123)
+    self.assertEqual(str(context.exception), "Please submit an array of elements")
+    
+  def test_extract_markdown_image(self):
+    node = TextNode("This is text with an image ![flower](flower.jpg)", TextType.PLAIN)
+    converted = split_nodes_image([node])
+    self.assertEqual(converted, [TextNode("This is text with an image ", TextType.PLAIN), TextNode("flower", TextType.IMAGE, "flower.jpg")])
+  
+      
+  def test_extract_markdown_images(self):
+    node = TextNode("This is text with an image ![flower](flower.jpg) and an image of the ![sun](sun.jpg)", TextType.PLAIN)
+    converted = split_nodes_image([node])
+    self.assertEqual(converted, [TextNode("This is text with an image ", TextType.PLAIN), TextNode("flower", TextType.IMAGE, "flower.jpg"), TextNode(" and an image of the ", TextType.PLAIN), TextNode("sun", TextType.IMAGE, "sun.jpg")])
+
+  def test_empty_images(self):
+    with self.assertRaises(Exception) as context:
+      split_nodes_image([])
+    self.assertEqual(str(context.exception), "Empty list not allowed!")
+    
+  def test_non_list_images(self):
+    with self.assertRaises(Exception) as context:
+      split_nodes_image(123)
+    self.assertEqual(str(context.exception), "Please submit an array of elements")
 if __name__ == "__main__":
   unittest.main()
